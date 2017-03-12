00caeb49502a6c40318dc7c6b3af417c54e37ef7
eatingbird <consulthere@gmail.com>
2017-03-12 13:04:32 -0700

    Added test file

--- server.py
+++ server.py
@@ -9,25 +9,29 @@ app = Flask(__name__)
 app.secret_key = "ABC"
 
 @app.route("/")
+
 def index():
     return render_template("index.html")
 
 @app.route("/application-form")
 def form():
+    """Show application form. Get the input by POST."""
+
     first = request.form.get('first')
     last = request.form.get('last')
     salary = request.form.get('salary')
     job = request.form.get('job')
 
-    return render_template("application-form.html", 
-                           first=first, last=last, salary=salary,
-                           job=job)
+    return render_template("application-form.html")
 
 
-@app.route("/application-success")
+@app.route("/application-success", methods=["POST"])
 def show_post():
+    """"""
 
-    return render_template('application-response.html')
+    return render_template('application-response.html',
+                           first=first, last=last,
+                           salary=salary, job=job)
 
 
 if __name__ == "__main__":
