f822128848e2d6bfc65f3b083a071e1b6d820fc7
eatingbird <consulthere@gmail.com>
2017-03-11 17:05:20 -0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+@app.route('/')
+    return renter_template(index.html)
+
+
+# YOUR ROUTES GO HERE
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
